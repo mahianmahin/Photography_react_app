dbd3a8b40378a6f3014c81d0d52cb088c84d6515
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-09-23 00:31:59 +0600

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import style from './navbar.module.css';
+
+const NavbarComponant = () => {
+    return (
+        <div className={`${style.main} d-flex flex-row-reverse`}>
+            <p>Navbar</p>
+        </div>
+    )
+};
+
+export default NavbarComponant;
